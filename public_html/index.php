418d21cc382da6afb3e8ffe69de5d5f8f8c0f5bb
carljasson <carlilustrisimo953@gmail.com>
2025-09-11 16:35:24 +0800

    git add . git commit -m "Move public to public_html for Hostinger" git push

---
+++ public_html/index.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Foundation\Application;
+use Illuminate\Http\Request;
+
+define('LARAVEL_START', microtime(true));
+
+// Determine if the application is in maintenance mode...
+if (file_exists($maintenance = __DIR__.'/../biometric-medical-access/storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register the Composer autoloader...
+require __DIR__.'/../biometric-medical-access/vendor/autoload.php';
+
+// Bootstrap Laravel and handle the request...
+/** @var Application $app */
+$app = require_once __DIR__.'/../biometric-medical-access/bootstrap/app.php';
+
+$response = $app->handle(
+    $request = Request::capture()
+);
+
+$response->send();
+
+$app->terminate($request, $response);
