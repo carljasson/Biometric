b2a7ada2ac605dc29f4dab0ea9ed3ec77a4f5c1b
carljasson <carlilustrisimo953@gmail.com>
2025-09-24 10:07:29 +0800

    update for database.php

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
+            'url' => env('https://biomedaccess.com'),
             'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'u802714156_biomedAccess'),
+            'username' => env('DB_USERNAME', 'u802714156_biomedAccessP'),
+            'password' => env('DB_PASSWORD', '1MedPass2025'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
