2e691d7a762bf0ea6cf37fbc3afb78d1e896d54f
carljasson <carlilustrisimo953@gmail.com>
2025-09-26 17:17:47 +0800

    test

--- test.php
+++ test.php
@@ -1,9 +1,2 @@
 <?php
-$link = mysqli_connect(
-    'mysql.hostinger.com',  // or whatever DB_HOST should be
-    'u802714156_biomedAccessP',
-    '1MedPass2025',
-    'u802714156_biomedAccess'
-);
-if(!$link) { die('Connect Error: '.mysqli_connect_error()); }
-echo '✅ DB Connected!';
+phpinfo();
