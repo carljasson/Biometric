c7fd6535a8cbc39cb5eca58b8e54752c6eb02378
carljasson <carlilustrisimo953@gmail.com>
2025-09-26 16:41:32 +0800

    add artisan-clear.php for clearing

---
+++ artisan-clear.php
@@ -0,0 +1,7 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+$app = require_once __DIR__.'/bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->call('config:clear');
+$app->make(Illuminate\Contracts\Console\Kernel::class)->call('cache:clear');
+$app->make(Illuminate\Contracts\Console\Kernel::class)->call('config:cache');
+echo "✅ Cache cleared!";
