d76a99615c2567c0c54ea36f0d135af432540f12
carljasson <carlilustrisimo953@gmail.com>
2025-09-29 10:37:25 +0800

    back index and htaccess

--- public/index.php
+++ public/index.php
@@ -2,9 +2,7 @@
 
 define('LARAVEL_START', microtime(true));
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+
 
 // Maintenance mode check
 if (file_exists(__DIR__.'/storage/framework/maintenance.php')) {
@@ -12,10 +10,10 @@ if (file_exists(__DIR__.'/storage/framework/maintenance.php')) {
 }
 
 // Composer autoload
-require __DIR__.'/vendor/autoload.php';
+require __DIR__.'/../vendor/autoload.php';
 
 // Bootstrap Laravel
-$app = require_once __DIR__.'/bootstrap/app.php';
+$app = require_once __DIR__.'/../bootstrap/app.php';
 
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
 
