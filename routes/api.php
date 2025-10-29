5efde9b6d719db9139a5eb062bec8fb9b40fb3a4
carljasson <carlilustrisimo953@gmail.com>
2025-10-30 01:51:25 +0800

    api

--- routes/api.php
+++ routes/api.php
@@ -6,26 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Models\User;
 
-Route::post('/identify-fingerprint', function (Request $request) {
-    $fingerprint = $request->input('fingerprint_template');
 
-    // decode base64
-    $binaryFingerprint = base64_decode($fingerprint);
-
-    // ⚠️ Use ZK SDK or your stored fingerprint comparison function
-    // For example, you can compare directly if you stored templates from zkfp2.DBMerge()
-    // Here we assume you have stored the merged fingerprint as base64 in `fingerprint_template` column
-
-    $users = User::whereNotNull('fingerprint_template')->get();
-
-    foreach ($users as $user) {
-        // Compare using your fingerprint library or an external script
-        // If match found:
-        // return user info
-    }
-
-    return response()->json(['error' => 'No match found'], 404);
-});
 
 Route::post('/register/web', [BiometricController::class, 'store']);
 Route::post('/register/api', [ApiRegisterController::class, 'store']);
