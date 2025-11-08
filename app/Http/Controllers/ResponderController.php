3fa8792c0d35706c8e831ec9c2c50dfa98e837f8
carljasson <carlilustrisimo953@gmail.com>
2025-11-08 12:06:11 +0800

    add in the responder

--- app/Http/Controllers/ResponderController.php
+++ app/Http/Controllers/ResponderController.php
@@ -9,7 +9,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use App\Models\User;
 use App\Models\Announcement;
-
+use Illuminate\Support\Facades\Http;
 
 
 class ResponderController extends Controller
