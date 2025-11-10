bbe2a8acb79474f6327d9ae32497c18771743ba8
carljasson <carlilustrisimo953@gmail.com>
2025-11-11 00:07:39 +0800

    admin id remove

--- app/Http/Middleware/AdminAuth.php
+++ app/Http/Middleware/AdminAuth.php
@@ -1,15 +1,15 @@
-<?php
-
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class AdminAuth
 {
     public function handle(Request $request, Closure $next)
     {
-        if (!session()->has('admin_id')) {
+        if (!Auth::guard('admin')->check()) {
+            // Allow login and register pages without redirect
             if ($request->is('admin/login') || $request->is('admin/register')) {
                 return $next($request);
             }
