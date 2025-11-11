e3df7f6d023ccd98adbef49bc64b33c72230188e
carljasson <carlilustrisimo953@gmail.com>
2025-11-12 01:20:37 +0800

    may ge delete

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -136,19 +136,6 @@
                             {{ Auth::user()->email ?? Auth::guard('admin')->user()->email ?? '' }}
                         </div>
                     </div>
-
-                    <div class="mt-3 space-y-1">
-                        <x-responsive-nav-link :href="route('profile.edit')">
-                            {{ __('Profile') }}
-                        </x-responsive-nav-link>
-
-                        <form method="POST" action="{{ route('logout') }}">
-                            @csrf
-                            <x-responsive-nav-link :href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();">
-                                {{ __('Log Out') }}
-                            </x-responsive-nav-link>
-                        </form>
-                    </div>
                 </div>
             </div>
 
